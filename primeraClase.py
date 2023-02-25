f86ccefe1cc99762ba8a0791753c4c6c95a6ba5b
unknown <jaidiberbernal@gmail.com>
2023-02-24 20:31:11 -0500

    comparacion entre cadenas de texto

--- primeraClase.py
+++ primeraClase.py
@@ -1,3 +1,13 @@
 #name = input("hola ingresa tu nombre: ")
-number = int(input("Ingrese la edad: "))
-print("Bienvenido ", number)
+#number = int(input("Ingrese la edad: "))
+#print("Bienvenido ", number)
+acrilico=input("que quiere")
+plata ="plata"
+noplata="noplata"
+def uñas (acrilico, plata, noplata):
+    if (acrilico == plata):
+        print ("Se ve bueno")
+    elif(acrilico == noplata):
+        print ("No se ve bueno")
+    
+uñas(acrilico, plata, noplata)
